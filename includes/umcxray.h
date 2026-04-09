4cb150f563b425bb55a25cdf91483253ec40b362
Mark Devenyi <markdevenyidev@gmail.com>
2026-04-09 13:22:47 -0400

    feat(reader): verify ioctl() status

--- includes/umcxray.h
+++ includes/umcxray.h
@@ -3,6 +3,7 @@
 
 #define UMC_MAGIC 0xAFA0
 #define UMCXRAY_VERSION "v1.0.0"
+#define UMCXRAY_DEV_PATH "/dev/umcxray"
 
 struct umc_regs {
     unsigned short magic;
--- reader/src/main.c
+++ reader/src/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #include "umcxray.h"
+#include "uxioctl.h"
 
 int main(void) {
-    printf("Hello World!\n");
+    struct uxioctl_req req = { 0 };
+
+    int fd = open(UMCXRAY_DEV_PATH, O_RDONLY);
+    if (fd == -1)
+    {
+        perror("open()");
+    }
+
+    if (ioctl(fd, UMCXRAY_GET_STATUS, &req) == -1) {
+        perror("ioctl(status)");
+    }
+
+    if (req.status != UMCXRAY_VALID_STATUS)
+    {
+        fprintf(stderr, "Error: IOCTL status was invalid!");
+    }
+
     return 0;
 }
